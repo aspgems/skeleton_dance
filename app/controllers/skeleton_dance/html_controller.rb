3a62fde10337f97afcc86215693593721187e768
Andrés Cirugeda <andres@aspgems.com>
2015-03-27 10:53:42 +0100

    Added support for html templates in engines

--- app/controllers/skeleton_dance/html_controller.rb
+++ app/controllers/skeleton_dance/html_controller.rb
@@ -14,6 +14,9 @@ class SkeletonDance::HtmlController < ActionController::Base
 
   def add_project_paths
     prepend_view_path Rails.root.join('app', 'assets', 'html')
+    Dir.glob(Rails.root.join('engines', '**/app/assets/html')).each do |engine_path|
+      prepend_view_path engine_path
+    end
   end
 
   def layout_param
